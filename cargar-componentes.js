31b1792b4e74978dbaf7da684c50a5a5fa0004f5
Spinelk <106118038+Spinelk@users.noreply.github.com>
2023-06-20 23:31:24 -0400

    Ahora el navbar se actualiza correctamente

--- cargar-componentes.js
+++ cargar-componentes.js
@@ -8,3 +8,20 @@ function loadComponent(url, containerId) {
     xhttp.open("GET", url, true);
     xhttp.send();
 }
+
+function loadComponent(url, containerId, elementId, className) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            var container = document.getElementById(containerId);
+            container.innerHTML += this.responseText;
+            var element = document.getElementById(elementId);
+            if (element) {
+                element.classList.add(className);
+            }
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
