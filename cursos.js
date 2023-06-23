dd21389bf0ce3794442931f8c4223d40fca3ad9a
Madafakki <josa.morales@duocuc.cl>
2023-06-23 15:38:52 -0400

    Arregle otra cosita falta (crear curso/hechizos, usuario, y mensajes))

--- cursos.js
+++ cursos.js
@@ -1,20 +1,3 @@
-// function mostrarContenido(idContenido) {
-//     var contenido = document.getElementById(idContenido);
-
-//     // Cambia la visibilidad del contenido
-//     if (contenido.style.display === 'none') {
-//         contenido.style.display = 'block';
-//     } else {
-//         contenido.style.display = 'none';
-//     }
-// }
-
-// function mostrarDetalle(idDetalle) {
-//     // Aquí puedes implementar la lógica para mostrar los detalles del contenido
-//     alert('Has hecho clic en el detalle ' + idDetalle);
-// }
-
-
 const cursos = [
     {
         id: 1,
@@ -137,3 +120,19 @@ function mostrarOcultarDetalle(event) {
 
 // Generar los cursos
 generarCursos();
+
+function mostrarContenido(idContenido) {
+    var contenido = document.getElementById(idContenido);
+
+    // Cambia la visibilidad del contenido
+    if (contenido.style.display === 'none') {
+        contenido.style.display = 'block';
+    } else {
+        contenido.style.display = 'none';
+    }
+}
+
+function mostrarDetalle(idDetalle) {
+    // Aquí puedes implementar la lógica para mostrar los detalles del contenido
+    alert('Has hecho clic en el detalle ' + idDetalle);
+}
