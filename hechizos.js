9c2e33362aaa3223251ca748f860760ac96b5aa4
Madafakki <josa.morales@duocuc.cl>
2023-06-23 11:14:27 -0400

    FILTRO DE HECHIZOS HECHO JUJUJUJUJU
    somos terriblo weno

--- hechizos.js
+++ hechizos.js
@@ -2,53 +2,61 @@
 
 const hechizos = [
 
-    {   
-        id:"01",
-        nombre:"Expelliarmus",
-        descripcion:"Hechizo Desarmador",
-        imagen:"img/hechizos/HM_Expelliarmus.webp"
+    {
+        id: "01",
+        nombre: "Expelliarmus",
+        descripcion: "Hechizo Desarmador",
+        imagen: "img/hechizos/HM_Expelliarmus.webp",
+        tipo: "ofensivo"
     },
-    {   
-        id:"02",
-        nombre:"Protego",
-        descripcion:"Hechizo protector",
-        imagen:"img/hechizos/HM_Protego.webp"
+    {
+        id: "02",
+        nombre: "Protego",
+        descripcion: "Hechizo protector",
+        imagen: "img/hechizos/HM_Protego.webp",
+        tipo: "defensivo"
     },
-    {   
-        id:"03",
-        nombre:"Incendio",
-        descripcion:"Hechizo de fuego",
-        imagen:"img/hechizos/HM_Incendio.webp"
+    {
+        id: "03",
+        nombre: "Incendio",
+        descripcion: "Hechizo de fuego",
+        imagen: "img/hechizos/HM_Incendio.webp",
+        tipo: "ofensivo"
     },
-    {   
-        id:"04",
-        nombre:"Reparo",
-        descripcion:"Hechizo Curativo",
-        imagen:"img/hechizos/HM_Reparo.webp"
+    {
+        id: "04",
+        nombre: "Reparo",
+        descripcion: "Hechizo Curativo",
+        imagen: "img/hechizos/HM_Reparo.webp",
+        tipo: "curativo"
     },
-    {   
-        id:"05",
-        nombre:"Wingardium Leviosa",
-        descripcion:"Hechizo levitatorio",
-        imagen:"img/hechizos/HM_Wingardium_Leviosa.webp"
+    {
+        id: "05",
+        nombre: "Wingardium Leviosa",
+        descripcion: "Hechizo levitatorio",
+        imagen: "img/hechizos/HM_Wingardium_Leviosa.webp",
+        tipo: "especial"
     },
-    {   
-        id:"06",
-        nombre:"Petrificus Totalus",
-        descripcion:"Hechizo paralizador",
-        imagen:"img/hechizos/HM_Petrificus_Totalus.webp"
+    {
+        id: "06",
+        nombre: "Petrificus Totalus",
+        descripcion: "Hechizo paralizador",
+        imagen: "img/hechizos/HM_Petrificus_Totalus.webp",
+        tipo: "ofensivo"
     },
-    {   
-        id:"07",
-        nombre:"Desmaius",
-        descripcion:"Hechizo aturdidor",
-        imagen:"img/hechizos/HM_Desmaius.webp"
+    {
+        id: "07",
+        nombre: "Desmaius",
+        descripcion: "Hechizo aturdidor",
+        imagen: "img/hechizos/HM_Desmaius.webp",
+        tipo: "furtivo"
     },
-    {   
-        id:"08",
-        nombre:"Lumos",
-        descripcion:"Hechizo de iluminación",
-        imagen:"img/hechizos/HM_Lumos.webp"
+    {
+        id: "08",
+        nombre: "Lumos",
+        descripcion: "Hechizo de iluminación",
+        imagen: "img/hechizos/HM_Lumos.webp",
+        tipo: "especial"
     },
 ]
 
@@ -60,9 +68,9 @@ function cargarHechizos() {
         const div = document.createElement("div");
         div.classList.add("hechizo")
         div.innerHTML = `
-        <div class="col">
+        <div class="row">
             <div
-                    class="card rounded-0 d-flex align-items-center"
+                    class="card mx-2 rounded-0 d-flex align-items-center"
                     style="width: 18rem"
                     id="${hechizo.id}"
                 >
@@ -72,12 +80,12 @@ function cargarHechizos() {
                         style="width: 75%"
                         alt="..."
                     />
-                    <div class="card-body row row-cols-1">
+                    <div class="card-body row ">
                         <h5 class="card-title" style="font-weight: bold">
                             ${hechizo.nombre}
                         </h5>
                         <p class="card-text">${hechizo.descripcion}</p>
-                        <a href="#" class="btn btn-primary mt-3 rounded-0"
+                        <a href="#" class="btn btn-primary mt-3 rounded-0 "
                             >Aprender</a
                         >
                     </div>
@@ -111,3 +119,83 @@ style="width: 18rem"
     >
 </div>
 </div> */}
+
+
+// Formula de filtrar hechizos
+//para los botones check
+const defensivoFilter = document.getElementById("check1");
+const ofensivoFilter = document.getElementById("check2");
+const furtivoFilter = document.getElementById("check3");
+const especialesFilter = document.getElementById("check4");
+const curativoFilter = document.getElementById("check5");
+
+defensivoFilter.addEventListener("click", filtrarHechizos);
+ofensivoFilter.addEventListener("click", filtrarHechizos);
+furtivoFilter.addEventListener("click", filtrarHechizos);
+especialesFilter.addEventListener("click", filtrarHechizos);
+curativoFilter.addEventListener("click", filtrarHechizos);
+
+function filtrarHechizos() {
+    const hechizosFiltrados = [];
+
+    if (defensivoFilter.checked) {
+        // Filtrar hechizos defensivos
+        hechizosFiltrados.push(...hechizos.filter(hechizo => /* Condición de filtro para hechizos defensivos */ hechizo.tipo === "defensivo"));
+    }
+
+    if (furtivoFilter.checked) {
+        // Filtrar hechizos furtivos
+        hechizosFiltrados.push(...hechizos.filter(hechizo => /* Condición de filtro para hechizos furtivos */hechizo.tipo === "furtivo"));
+    }
+    if (ofensivoFilter.checked) {
+        // Filtrar hechizos furtivos
+        hechizosFiltrados.push(...hechizos.filter(hechizo => /* Condición de filtro para hechizos furtivos */hechizo.tipo === "ofensivo"));
+    }
+    if (especialesFilter.checked) {
+        // Filtrar hechizos furtivos
+        hechizosFiltrados.push(...hechizos.filter(hechizo => /* Condición de filtro para hechizos furtivos */hechizo.tipo === "especial"));
+    }
+    if (curativoFilter.checked) {
+        // Filtrar hechizos furtivos
+        hechizosFiltrados.push(...hechizos.filter(hechizo => /* Condición de filtro para hechizos furtivos */hechizo.tipo === "curativo"));
+    }
+
+    // Limpiar el contenedor de hechizos antes de mostrar los resultados filtrados
+    contenedorHechizos.innerHTML = "";
+
+    if (hechizosFiltrados.length > 0) {
+        hechizosFiltrados.forEach(hechizo => {
+            // Crear y agregar elementos de hechizos filtrados al contenedor, haremos la misma creacion de cards :3
+            const div = document.createElement("div");
+            // ... código para crear el elemento de hechizo filtrado
+            div.innerHTML = `
+        <div class="row">
+            <div
+                    class="card mx-2 rounded-0 d-flex align-items-center"
+                    style="width: 18rem"
+                    id="${hechizo.id}"
+                >
+                    <img
+                        src="${hechizo.imagen}"
+                        class="card-img-top mt-4 mb-2"
+                        style="width: 75%"
+                        alt="..."
+                    />
+                    <div class="card-body row ">
+                        <h5 class="card-title" style="font-weight: bold">
+                            ${hechizo.nombre}
+                        </h5>
+                        <p class="card-text">${hechizo.descripcion}</p>
+                        <a href="#" class="btn btn-primary mt-3 rounded-0 "
+                            >Aprender</a
+                        >
+                    </div>
+            </div>
+        </div>
+        `;
+            contenedorHechizos.appendChild(div);
+        });
+    } else {
+        cargarHechizos();
+    }
+}
